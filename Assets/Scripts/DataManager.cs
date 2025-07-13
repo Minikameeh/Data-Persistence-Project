3ea89507845e09e1441602ea512d2b5fe1103288
Minikameeh <adri.jimenez.aliste@gmail.com>
2025-07-13 19:54:23 +0200

    recconnecting the UI

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -16,15 +16,20 @@ public class DataManager : MonoBehaviour
     public TMP_InputField nameInputField;
     public TextMeshProUGUI bestScoreTextMenu;
     private string saveFilePath;
-    public GameObject ErasePoPUp;
-    public GameObject LeaderBoardScreen;
-    public Button startButton;
+    [SerializeField] public GameObject ErasePoPUp;
+    [SerializeField] public GameObject LeaderBoardScreen;
+    [SerializeField] public Button startButton;
+    [SerializeField] public Button eraseButton;
+    [SerializeField] public Button eraseButtonYes;
+    [SerializeField] public Button eraseButtonNo;
+    [SerializeField] public Button leaderBoardButtonScreen;
+    [SerializeField] public Button leaderBoardButtonQuit;
     [SerializeField] public string playerName;
     public MainManager mainManager;
     public static DataManager Instance;
+    public int activePlayerIndex = -1;
     public int bestScore;
     public PlayerList playerList = new PlayerList();
-    public int activePlayerIndex = -1;
 
     private void Awake()
     {
@@ -49,29 +54,30 @@ public class DataManager : MonoBehaviour
         ErasePoPUp.SetActive(false);
         saveFilePath = Application.persistentDataPath + "/playerData.Json";
         LoadData();
-        if (nameInputField == null)
-            nameInputField = GameObject.Find("NameInputField")?.GetComponent<TMP_InputField>();
-        if (startButton == null)
-            startButton = GameObject.Find("StartButton")?.GetComponent<Button>();
-
-        // Asegúrate de limpiar antes de volver a añadir el listener
-        if (startButton != null)
-        {
-            startButton.onClick.RemoveAllListeners();
-            startButton.onClick.AddListener(StartGame);
-        }
+        ReconnectUI();
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (nameInputField != null)
         {
             playerName = nameInputField.text;
             bestScoreTextMenu.text = "Best Score: " + playerName + " : " + bestScore;
         }
-       
+        eraseButton.onClick.AddListener(EraseDataPopUp);
+        eraseButtonYes.onClick.AddListener(EraseYesButton);
+        eraseButtonNo.onClick.AddListener(EraseNoButton);
+        leaderBoardButtonScreen.onClick.AddListener(LeaderBoardScreenPopUp);
+
+
+
+
+
+
     }
 
     public void StartGame()
@@ -197,6 +203,12 @@ public class DataManager : MonoBehaviour
     public void LeaderBoardScreenPopUp()
     {
         LeaderBoardScreen.SetActive(true);
+        leaderBoardButtonScreen.gameObject.SetActive(false);
+    }
+    public void LeaderBoardScreenQuit()
+    {
+        LeaderBoardScreen.SetActive(false);
+        leaderBoardButtonScreen.gameObject.SetActive(true);
     }
 
     public void DeleteAllData()
@@ -226,9 +238,38 @@ public class DataManager : MonoBehaviour
             .OrderByDescending(p => p.bestScore)
             .ToList();
     }
+    private void ReconnectUI()
+    {
+        nameInputField = GameObject.Find("Player Name")?.GetComponent<TMP_InputField>();
+        bestScoreTextMenu = GameObject.Find("Best Score Menu Text")?.GetComponent<TextMeshProUGUI>();
+        startButton = GameObject.Find("Start Button")?.GetComponent<Button>();
+        eraseButton = GameObject.Find("Erase Data Button")?.GetComponent<Button>();
+        eraseButtonYes = GameObject.Find("Yes")?.GetComponent<Button>();
+        eraseButtonNo = GameObject.Find("No")?.GetComponent<Button>();
+        leaderBoardButtonScreen = GameObject.Find("LeaderBoard Button")?.GetComponent<Button>();
+        leaderBoardButtonQuit = GameObject.Find("LeaderBoardQuitButton")?.GetComponent<Button>();
+
+        if (startButton != null)
+            startButton.onClick.AddListener(StartGame);
+
+        if (eraseButton != null)
+            eraseButton.onClick.AddListener(EraseDataPopUp);
+
+        if (eraseButtonYes != null)
+            eraseButtonYes.onClick.AddListener(EraseYesButton);
+
+        if (eraseButtonNo != null)
+            eraseButtonNo.onClick.AddListener(EraseNoButton);
+
+        if (leaderBoardButtonScreen != null)
+            leaderBoardButtonScreen.onClick.AddListener(LeaderBoardScreenPopUp);
+
+        if (leaderBoardButtonQuit != null)
+            leaderBoardButtonQuit.onClick.AddListener(LeaderBoardScreenQuit);
+    }
     
     
-    
+
 }
 
 
